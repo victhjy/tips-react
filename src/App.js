bb889610c946e595237d744acfc830351f9c1fc8
jinyang.huang <jinyang.huang@alibaba-inc.com>
2021-04-09 17:20:25 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Welcome(props){
+  return <h5 className="Welcome">Hello, {props.name}</h5>
+}
+
+function Tick(){
+  return(
+    <div>
+      <h1>Time</h1>
+      <h2>It's {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+}
+
+const div1 = {
+  // width: "300px",
+  margin: "10px",
+  // minHeight: "200px",
+  // boxSizing: "border-box"
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {div1}>
+          <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0"></meta>
+      <button style={{color:"red"}}>Click</button>
+      <Welcome name="Sara"/>
+      <Welcome name="Lily"/>
+      <Welcome name="Ran"/>
+      <Tick/>
     </div>
   );
 }
+
+
+
+// setInterval(App, 1000);
+
 
 export default App;
