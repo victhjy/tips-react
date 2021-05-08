9175c5fb0f5b17a8df2f942acbb97ab670390cbd
jinyang.huang <jinyang.huang@alibaba-inc.com>
2021-05-08 17:36:25 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import './App.css';
-
-function Welcome(props){
-  return <h5 className="Welcome">Hello, {props.name}</h5>
-}
-
-function Tick(){
-  return(
-    <div>
-      <h1>Time</h1>
-      <h2>It's {new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-}
-
-const div1 = {
-  // width: "300px",
-  margin: "10px",
-  // minHeight: "200px",
-  // boxSizing: "border-box"
-};
+import HeaderComponent from './Components/HeaderComponent'
 
 function App() {
   return (
-    <div style = {div1}>
-          <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0"></meta>
-      <button style={{color:"red"}}>Click</button>
-      <Welcome name="Sara"/>
-      <Welcome name="Lily"/>
-      <Welcome name="Ran"/>
-      <Tick/>
+    <div>
+      <HeaderComponent></HeaderComponent>
     </div>
   );
 }
-
-
-
-// setInterval(App, 1000);
 
 
 export default App;
---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+export default
+class HeaderComponent extends React.Component{
+    constructor(props){
+        super(props)
+        this.theLi = React.createRef()
+    }
+    state = {
+        "l":[1,2,34],
+        "r":[11,22,33]
+    }
+    
+    aaa(val){
+        // alert(this.refs.theLi)
+        console.log(this.theLi.current)
+        console.log(val)
+        alert(val)
+    }
+    render() {
+       return(
+           <div>
+               <ul ref={this.theLi}>
+               {
+                this.state.l.map((val)=>{
+                    return <li onClick={()=>{this.aaa(val)}} >{val}</li>
+                })
+               }
+               </ul>
+           </div>
+       ) 
+    }
+}
