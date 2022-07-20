e7d08471ac4f51e27f6f7d07f56c89eccf3c3caf
jinyang.huang <jinyang.huang@alibaba-inc.com>
2022-07-20 18:09:26 +0800

    0720

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,32 +1,34 @@
 import React from 'react'
 export default
-class HeaderComponent extends React.Component{
-    constructor(props){
+class HeaderComponent extends React.Component {
+    constructor(props) {
         super(props)
         this.theLi = React.createRef()
     }
     state = {
-        "l":[1,'ss',34],
-        "r":[11,22,33]
+        "l": [1, 'ss 20220720', 34],
+        "r": [11, 22, 33]
     }
-    
-    aaa(val){
+
+    aaa(val) {
         // alert(this.refs.theLi)
         console.log(this.theLi.current)
         console.log(val)
         alert(val)
     }
     render() {
-       return(
-           <div id="nav" >
-               <ul ref={this.theLi}>
-               {
-                this.state.l.map((val)=>{
-                    return <li onClick={()=>{this.aaa(val)}} >{val}</li>
+        return ( <
+            div id = "nav" >
+            <
+            ul ref = { this.theLi } > {
+                this.state.l.map((val) => {
+                    return <li onClick = {
+                        () => { this.aaa(val) }
+                    } > { val } < /li>
                 })
-               }
-               </ul>
-           </div>
-       ) 
+            } <
+            /ul> < /
+            div >
+        )
     }
 }
