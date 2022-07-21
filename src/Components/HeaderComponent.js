bc38f616c74b349635c34545f738b42f215cfd79
victhjy <victhjycn@gmail.com>
2022-07-21 15:14:39 +0800

    1

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,34 +1,18 @@
-import React from 'react'
-export default
-class HeaderComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.theLi = React.createRef()
-    }
-    state = {
-        "l": [1, 'ss 20220720', 34],
-        "r": [11, 22, 33]
-    }
-
-    aaa(val) {
-        // alert(this.refs.theLi)
-        console.log(this.theLi.current)
-        console.log(val)
-        alert(val)
-    }
-    render() {
-        return ( <
-            div id = "nav" >
-            <
-            ul ref = { this.theLi } > {
-                this.state.l.map((val) => {
-                    return <li onClick = {
-                        () => { this.aaa(val) }
-                    } > { val } < /li>
-                })
-            } <
-            /ul> < /
-            div >
-        )
-    }
+import React from "react";
+export default class HeaderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.theLi = React.createRef();
+  }
+  state = {
+    l: [1, "ss 20220720", 34],
+    r: [11, 22, 33],
+  };
+  render() {
+    return (
+      <div id="nav">
+        Hello{this.state.l[0]}
+      </div>
+    );
+  }
 }
--- src/Components/MainContentComponent.js
+++ src/Components/MainContentComponent.js
@@ -1,12 +1,12 @@
-import React from 'react'
-export default
-class MainContentComponent extends React.Component {
-    render() {
-        return ( <
-            div id = "content" >
+import React from "react";
+export default class MainContentComponent extends React.Component {
+    state = {
+        curTime:new Date().toLocaleDateString()
+      }
 
-            <
-            /div>
-        )
-    }
+  render() {
+    return <div id="content">
+        <div>访问日期  {this.state.curTime}</div>
+    </div>;
+  }
 }
